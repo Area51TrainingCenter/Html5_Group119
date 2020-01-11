288d689d3dc6b4a1ee82c6159fa4aa3755ee8c9a
alumno <alumno@MAC-87.local>
2020-01-11 12:07:37 -0500

    clase10

--- modulo3/clase10/js/lightbox.js
+++ modulo3/clase10/js/lightbox.js
@@ -6,12 +6,17 @@ jQuery(document).ready(function(){
 		var ruta = jQuery(this).attr("href");
 
 		jQuery(".contenedor-img img").attr("src",ruta);
-		jQuery(".trama").attr("class","trama abierto");
-		jQuery(".contenedor-img").attr("class","contenedor-img abierto");
+		// jQuery(".trama").attr("class","trama abierto");
+		// jQuery(".contenedor-img").attr("class","contenedor-img abierto");
+		jQuery(".trama").addClass("abierto");
+		jQuery(".contenedor-img").addClass("abierto");
+		
 	});
 
 	jQuery(".cerrar").click(function(){
-		jQuery(".trama").attr("class","trama");
-		jQuery(".contenedor-img").attr("class","contenedor-img");
+	// 	jQuery(".trama").attr("class","trama");
+	// 	jQuery(".contenedor-img").attr("class","contenedor-img");
+		jQuery(".trama").removeClass("abierto");
+		jQuery(".contenedor-img").removeClass("abierto");
 	});
 });

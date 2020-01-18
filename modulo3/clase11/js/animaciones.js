a15136f860ced34042d730fc4e44d745a76491fe
Alonso Perez <nuevocorreo17@gmail.com>
2020-01-18 14:51:02 -0500

    clase11

--- modulo3/clase11/js/animaciones.js
+++ modulo3/clase11/js/animaciones.js
@@ -1,13 +1,53 @@
-jQuery(document).ready(function(){
-	jQuery(".cuadrado").click(function(){
-		var cuadrado = jQuery(this);
+var ancho = jQuery(window).width() - jQuery(".cuadrado").width() ;
+var alto = jQuery(window).height() - jQuery(".cuadrado").height() ;
 
-		cuadrado.animate({
-			"width": "+=200",
-			"height":"+=200",
-			"background-color":"#ff0000"
-		},2000,"linear",function(){
-			// alert("termino");
-		});
+
+jQuery(document).ready(function(){
+	// jQuery(".cuadrado").click(function(){
+	// 	var cuadrado = jQuery(this);
+
+	// 	cuadrado.animate({
+	// 		"width": "+=200",
+	// 		"height":"+=200",
+	// 		"background-color":"#ff0000"
+	// 	},2000,"linear",function(){
+	// 		// alert("termino");
+	// 	});
+	// });
+	jQuery(".cuadrado").css({
+		"width" : "80px",
+		"height" : "80px"
 	});
+	empezar();
+});
+
+function empezar()
+{
+	var cuadrado = jQuery(".cuadrado");
+	cuadrado.animate({
+		"left": ancho,
+		"background-color":"#ff0000"
+	},2000,"linear",function(){
+		cuadrado.animate({
+			"top": alto,
+			"background-color":"#0000ff"
+		},2000,"linear",function(){
+			cuadrado.animate({
+			"left": 0,
+			"background-color":"yellow"
+			},2000,"linear",function(){
+				cuadrado.animate({
+				"top": 0,
+				"background-color":"#00ff00"
+				},2000,"linear",function(){
+					empezar();
+				});				
+			});	
+		});	
+	});
+}
+
+jQuery(window).resize(function(){
+	ancho = jQuery(window).width() - jQuery(".cuadrado").width() ;
+	alto = jQuery(window).height() - jQuery(".cuadrado").height() ;
 });
--- modulo3/clase11/taller/js/funciones.js
+++ modulo3/clase11/taller/js/funciones.js
@@ -1,21 +1,81 @@
 jQuery(document).ready(function(){
-	jQuery(".hamb").click(function(){
-		jQuery("header.cabecera .menu").toggleClass("abierto");
-		jQuery(".hamb i").toggleClass("fa-times");
+	jQuery(".hamb").click(function(e){
+		e.preventDefault();
+		abrircerrar();
 	});
+
+	jQuery(".menu a[href^='#']").click(function(e){
+		e.preventDefault();
+		abrircerrar();
+		var ancla = jQuery(this).attr("href");
+		
+		if(jQuery(ancla).length > 0)
+		{
+			jQuery("html,body").animate({
+				"scrollTop":jQuery(ancla).offset().top
+			},1000,"linear");
+		}
+	});
+
+	jQuery(".saltarina").click(function(e){
+		e.preventDefault();
+
+		var saltarina = jQuery(this);
+		var ancla = saltarina.closest("section").next("section").offset().top;
+		jQuery("html,body").animate({
+			"scrollTop":ancla
+		},1000,"linear");
+	})
+
+
 });
 
+function abrircerrar()
+{
+	jQuery("header.cabecera .menu").toggleClass("abierto");
+	jQuery(".hamb i").toggleClass("fa-times");
+}
 
+//Load: metodo de carga de la web
 jQuery(window).on("load",function(){
 	jQuery(".precarga").fadeOut("slow");
 	var alto = jQuery("header").height();
 	var altointerno = jQuery("header").innerHeight();
 	//true es para obtener el alto con margen
 	var aloexterno = jQuery("header").outerHeight(true);
-	console.log(alto+" "+altointerno+" "+aloexterno);
+	// console.log(alto+" "+altointerno+" "+aloexterno);
+
+	//obtener el hash de la url
+	if(window.location.hash)
+	{
+		var ancla = window.location.hash;
+		if(jQuery(ancla).length > 0)
+		{
+			setTimeout(function(){
+				
+				jQuery("html,body").animate({
+					"scrollTop":jQuery(ancla).offset().top
+				},1000,"linear");
+			},1000);
+		}
+		
+	}
 });
 
 
+//Metodo scroll
+jQuery(window).scroll(function(){
+	//scrolltop: punto donde se ubica luego del scroll
+	if(jQuery(window).scrollTop() > 100)
+	{
+		jQuery("header").addClass("nombre");
+	}else{
+		jQuery("header").removeClass("nombre");
+	}
+});
+
+
+//Resize: evento para detectar si la ventana tuvo cambio de tamaño
 jQuery(window).resize(function(){
 	console.log(jQuery(window).width());
 });
